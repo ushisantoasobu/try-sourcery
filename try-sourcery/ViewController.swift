2fe6aba2eb6d75d95cba35d2b121a282e2f29021
ushisantoasobu <babblemann.shunsee@gmail.com>
2020-07-18 15:18:00 +0900

    READMEの例がいまいちと思ったのでちょっと確認用コードを書いてみた

--- try-sourcery/ViewController.swift
+++ try-sourcery/ViewController.swift
@@ -13,8 +13,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        let personA = Person(firstName: "Shunsuke", lastName: "Sato", birthDate: Date())
+        let personB = Person(firstName: "Taro", lastName: "Tanaka", birthDate: Date())
+
+        print(personA == personB)
     }
 
 
 }
 
+struct Person: Equatable {
+    let firstName: String
+    let lastName: String
+    let birthDate: Date
+}
+
+// https://github.com/krzysztofzablocki/Sourcery#tldr
+// READMEの例
+// でもこれってもうSwiftの言語仕様的に不要な例？？
+/*
+extension Person: Equatable {
+    static func ==(lhs: Person, rhs: Person) -> Bool {
+        guard lhs.firstName == rhs.firstName else { return false }
+        guard lhs.lastName == rhs.lastName else { return false }
+        guard lhs.birthDate == rhs.birthDate else { return false }
+        return true
+    }
+}
+*/
