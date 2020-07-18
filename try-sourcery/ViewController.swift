5388693938aecff86ff418f0d24ef56a67d93920
ushisantoasobu <babblemann.shunsee@gmail.com>
2020-07-18 15:12:31 +0900

    add project

---
+++ try-sourcery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  try-sourcery
+//
+//  Created by Shunsuke Sato on 2020/07/18.
+//  Copyright © 2020 Shunsuke Sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ try-sourceryTests/try_sourceryTests.swift
@@ -0,0 +1,34 @@
+//
+//  try_sourceryTests.swift
+//  try-sourceryTests
+//
+//  Created by Shunsuke Sato on 2020/07/18.
+//  Copyright © 2020 Shunsuke Sato. All rights reserved.
+//
+
+import XCTest
+@testable import try_sourcery
+
+class try_sourceryTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
